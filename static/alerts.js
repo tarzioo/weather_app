db75625e31b5470588cc9d3d970d89f2f5269f9a
Taraneh Tayaran <ttayaran@gmail.com>
2016-08-24 03:25:09 +0000

    Cleaned up formatting in alerts.js

--- static/alerts.js
+++ static/alerts.js
@@ -5,11 +5,11 @@ function getAlerts() {
     $.get('/alerts.json', function (response) {
         
         console.log(response);
-        $('#alerts').html("Temperature: " + response['apparentTemperature'] + "<br>" +
-                            "Humidity: " + response['humidity'] + "<br>" +
-                            "Nearest Storm Bearing: " + response["nearestStormBearing"] + "<br>" +
-                            "Nearest Storm Distance: " + response["nearestStormDistance"] + "<br>" +
-                            "Summary: " + response["summary"] + "<br>" 
+        $('#alerts').html("Temperature: " + response.apparentTemperature + "<br>" +
+                            "Humidity: " + response.humidity + "<br>" +
+                            "Nearest Storm Bearing: " + response.nearestStormBearing + "<br>" +
+                            "Nearest Storm Distance: " + response.nearestStormDistance + "<br>" +
+                            "Summary: " + response.summary + "<br>" 
                             );  
 
     });
