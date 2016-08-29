aedbb7674a84320b476ae36b9406020d476d614a
Taraneh Tayaran <ttayaran@gmail.com>
2016-08-29 16:54:09 +0000

    modified alertDetails.js to show icon in base.html to show alert type

--- static/alertDetails.js
+++ static/alertDetails.js
@@ -3,12 +3,28 @@
 
 function getAlertDetails(evt) {
     evt.preventDefault();
+    var url;
 
     $.get('/alert-details.json', function (response) {
         console.log(response);
+        
             if(response.alertType !== undefined){
+                if(response.alertType == "thunderstorm") {
+                    url = "<img src='/static/img/thunderstorm-icon.png'/>";
+                }
+                else if(response.alertType == "flood") {
+                    url = "<img src='/static/img/flood-icon.png'/>";
+                }
+                else if(response.alertType == "tornado") {
+                    url = "<img src='/static/img/tornado-icon.png'/>";
+
+                }
                 $('#alert-details').html("You are in a " + response.alertType +
-                                            " " + response.alertLevel );
+                                            " " + response.alertLevel +
+                                            url
+                                            );
+
+
             }
             else {
                 $("#alert-details").html(response.alertType);
@@ -17,3 +33,5 @@ function getAlertDetails(evt) {
 }
 
 $(document).on('ready', getAlertDetails);
+
+
