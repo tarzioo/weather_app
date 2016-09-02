c1f5f62ecc2494be95130272eced0ce1653def27
Taraneh Tayaran <ttayaran@gmail.com>
2016-09-02 19:27:37 +0000

    Created basic layout for tests.py

---
+++ tests.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+from model import *
+from server import app
+import server
+
+def setUp(self):
+    """Stuff to do before every test"""
+
+    #Get the Flask test client
+    self.client = app.test_client()
+
+    #Show Flask errors that happen during tests
+    app.config["TESTING"] = True
+
+    #Connect to test database
+
+    connect_to_db(app, "postgresql:///testdb")
+
+    #Create tables and add sample data
+    db.create_all()
+    example_data()
+
+    #Make mock
+
+    def 
+
+    def tearDown(self):
+        """Do at end of every test"""
+
+        db.session.close()
+        db.drop_all()
+
+        
+
+
+
+
+
+
+if __name__ == "__main__":
+    import unittest
+
+    unittest.main()
